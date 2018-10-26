25315211ab6a6ff4e9ea2f8408c0c2bfb0f04d15
matheus.lopes <matheus.lopes@softplan.com.br>
2018-10-26 18:25:58 -0300

    Convertendo texto em fala com a API Speech to Text IBM, Iniciando chat

--- SpeechToText/src/main/java/Application.java
+++ SpeechToText/src/main/java/Application.java
@@ -1,23 +1,52 @@
+import java.io.File;
 import java.io.FileNotFoundException;
 
-import javax.sound.sampled.AudioFormat;
-import javax.sound.sampled.AudioInputStream;
 import javax.sound.sampled.AudioSystem;
-import javax.sound.sampled.DataLine;
-import javax.sound.sampled.TargetDataLine;
-
-import com.ibm.watson.developer_cloud.http.HttpMediaType;
-import com.ibm.watson.developer_cloud.speech_to_text.v1.SpeechToText;
-import com.ibm.watson.developer_cloud.speech_to_text.v1.model.RecognizeOptions;
-import com.ibm.watson.developer_cloud.speech_to_text.v1.model.SpeechRecognitionResults;
-import com.ibm.watson.developer_cloud.speech_to_text.v1.websocket.BaseRecognizeCallback;
+import javax.sound.sampled.Clip;
 
 public class Application {
     
     public static void main(String[] args) throws FileNotFoundException, InterruptedException, Exception {
-        SpeechToText service = new SpeechToText();
+        /**
+         * TODO: Text to speech
+         */
+        /*TextToSpeech textToSpeech = new TextToSpeech();
+        textToSpeech.setUsernameAndPassword("eb657d72-4c20-4d27-b0b2-03bd2711f365", "60A18b20tDmz");
+        
+        try {
+            SynthesizeOptions synthesizeOptions = new SynthesizeOptions.Builder().text("Obrigada pelas informações.").accept("audio/wav")
+                .voice(Voice.PT_BR_ISABELAVOICE).build();
+            
+            InputStream inputStream = textToSpeech.synthesize(synthesizeOptions).execute();
+            InputStream in = WaveUtils.reWriteWaveHeader(inputStream);
+            
+            OutputStream out = new FileOutputStream("test11.wav");
+            byte[] buffer = new byte[1024];
+            int length;
+            while ((length = in.read(buffer)) > 0) {
+                out.write(buffer, 0, length);
+            }
+            
+            out.close();
+            in.close();
+            inputStream.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }*/
+        
+        /**
+         * TODO: Chat
+         */
+        
+        File file = new File("1.wav");
+        playSound(file);
+        
+        /**
+         * TODO: Speech to text
+         */
+       /* SpeechToText service = new SpeechToText();
         service.setUsernameAndPassword("8ef046d2-0b24-45c1-b12d-8b9e37cf535a", "w0jWHgRbtg46");
-
+        
         // Signed PCM AudioFormat with 16kHz, 16 bit sample size, mono
         int sampleRate = 16000;
         AudioFormat format = new AudioFormat(sampleRate, 16, 1, true, false);
@@ -33,21 +62,19 @@ public class Application {
         line.start();
         
         AudioInputStream audio = new AudioInputStream(line);
-
-        RecognizeOptions options = new RecognizeOptions.Builder()
-            .interimResults(true)
+        
+        RecognizeOptions options = new RecognizeOptions.Builder().interimResults(true)
             // .inactivityTimeout(5) // use this to stop listening when the speaker pauses, i.e. for 5s
-          .audio(audio)
-            .contentType(HttpMediaType.AUDIO_RAW + "; rate=" + sampleRate)
-          .build();
-
+            .audio(audio).contentType(HttpMediaType.AUDIO_RAW + "; rate=" + sampleRate).build();
+        
         service.recognizeUsingWebSocket(options, new BaseRecognizeCallback() {
-          @Override
-          public void onTranscription(SpeechRecognitionResults speechResults) {
-            System.out.println(speechResults);
-          }
+            
+            @Override
+            public void onTranscription(SpeechRecognitionResults speechResults) {
+                System.out.println(speechResults);
+            }
         });
-
+        
         System.out.println("Listening to your voice for the next 30s...");
         Thread.sleep(30 * 1000);
         
@@ -55,7 +82,18 @@ public class Application {
         line.stop();
         line.close();
         
-        System.out.println("Fin.");
+        System.out.println("Fin.");*/
     }
     
+    static void playSound(File sound) {
+        try {
+            Clip clip = AudioSystem.getClip();
+            clip.open(AudioSystem.getAudioInputStream(sound));
+            clip.start();
+            
+            Thread.sleep(clip.getMicrosecondLength() / 1000);
+        } catch (Exception e) {
+            
+        }
+    }
 }
