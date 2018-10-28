945bf29ce488dba3d69735e99aaa9d30736de679
MatheusTabares <tabares.lopes@gmail.com>
2018-10-28 05:02:35 -0300

    Variaveis estaticas

--- SpeechToText/src/main/java/Application.java
+++ SpeechToText/src/main/java/Application.java
@@ -30,10 +30,21 @@ public class Application {
 
 	public static File welcome = new File("welcome.wav");
 	
+	public static String VERSION_ASSISTENT = "2018-02-16";
+	public static String USER_WATSON_SPEECH_TO_TEXT = "8ef046d2-0b24-45c1-b12d-8b9e37cf535a";
+	public static String PASSWORD_WATSON_SPEECH_TO_TEXT = "w0jWHgRbtg46";
+	public static String USER_WATSON_ASSISTENT = "f17196fb-ad7f-4efe-b9bd-d7468f0a55b4";
+	public static String PASSWORD_WATSON_ASSISTENT = "fVChilOOMMNx";
+	public static String WORKSPACE_ID_WATSON_ASSISTENT = "f4067fdf-826c-4d91-ab91-b095f2cbb6ea";
+	public static String USER_WATSON_TEXT_TO_SPEECH = "eb657d72-4c20-4d27-b0b2-03bd2711f365";
+	public static String PASSWORD_WATSON_TEXT_TO_SPEECH = "60A18b20tDmz";
+	public static String LANGUAGE = "pt-BR_BroadbandModel";
+	
+	
 	public static void main(String[] args) throws FileNotFoundException, InterruptedException, Exception {
     	
     	SpeechToText service = new SpeechToText();
-        service.setUsernameAndPassword("8ef046d2-0b24-45c1-b12d-8b9e37cf535a", "w0jWHgRbtg46");
+        service.setUsernameAndPassword(USER_WATSON_SPEECH_TO_TEXT, PASSWORD_WATSON_SPEECH_TO_TEXT);
         
         int sampleRate = 16000;
         AudioFormat format = new AudioFormat(sampleRate, 16, 1, true, false);
@@ -51,7 +62,7 @@ public class Application {
         AudioInputStream audio = new AudioInputStream(line);
         
         RecognizeOptions options = new RecognizeOptions.Builder().interimResults(true)
-            .audio(audio).contentType(HttpMediaType.AUDIO_RAW + "; rate=" + sampleRate).model("pt-BR_BroadbandModel").build();
+            .audio(audio).contentType(HttpMediaType.AUDIO_RAW + "; rate=" + sampleRate).model(LANGUAGE).build();
         
         playSound(welcome);
         
@@ -61,17 +72,17 @@ public class Application {
             	if(speechResults.getResults().get(0).isFinalResults()) {
             		final String result = speechResults.getResults().get(0).getAlternatives().get(0).getTranscript().trim();
 	
-	            	Assistant serviceA = new Assistant("2018-02-16");
-	            	serviceA.setUsernameAndPassword("f17196fb-ad7f-4efe-b9bd-d7468f0a55b4", "fVChilOOMMNx");
+	            	Assistant serviceA = new Assistant(VERSION_ASSISTENT);
+	            	serviceA.setUsernameAndPassword(USER_WATSON_ASSISTENT, PASSWORD_WATSON_ASSISTENT);
 	
 	            	InputData input = new InputData.Builder(result).build();
-	            	MessageOptions option = new MessageOptions.Builder("f4067fdf-826c-4d91-ab91-b095f2cbb6ea")
+	            	MessageOptions option = new MessageOptions.Builder(WORKSPACE_ID_WATSON_ASSISTENT)
 	            	  .input(input)
 	            	  .build();
 	            	MessageResponse response = serviceA.message(option).execute();
 	            	
 	            	TextToSpeech textToSpeech = new TextToSpeech();
-	                textToSpeech.setUsernameAndPassword("eb657d72-4c20-4d27-b0b2-03bd2711f365", "60A18b20tDmz");
+	                textToSpeech.setUsernameAndPassword(USER_WATSON_TEXT_TO_SPEECH, PASSWORD_WATSON_TEXT_TO_SPEECH);
 	                
 	                try {
 	                    SynthesizeOptions synthesizeOptions = new SynthesizeOptions.Builder().text(response.getOutput().getText().toString()).accept("audio/wav")
