85dd3a1a2098449fe6ebcf390498dc15b0480444
MatheusTabares <tabares.lopes@gmail.com>
2018-10-28 00:36:37 -0300

    adicionado fluxo temporario

--- SpeechToText/src/main/java/Application.java
+++ SpeechToText/src/main/java/Application.java
@@ -8,6 +8,11 @@ import javax.sound.sampled.Clip;
 import javax.sound.sampled.DataLine;
 import javax.sound.sampled.TargetDataLine;
 
+import com.ibm.watson.developer_cloud.assistant.v1.Assistant;
+import com.ibm.watson.developer_cloud.assistant.v1.model.Context;
+import com.ibm.watson.developer_cloud.assistant.v1.model.InputData;
+import com.ibm.watson.developer_cloud.assistant.v1.model.MessageOptions;
+import com.ibm.watson.developer_cloud.assistant.v1.model.MessageResponse;
 import com.ibm.watson.developer_cloud.http.HttpMediaType;
 import com.ibm.watson.developer_cloud.speech_to_text.v1.SpeechToText;
 import com.ibm.watson.developer_cloud.speech_to_text.v1.model.RecognizeOptions;
@@ -15,6 +20,12 @@ import com.ibm.watson.developer_cloud.speech_to_text.v1.model.SpeechRecognitionR
 import com.ibm.watson.developer_cloud.speech_to_text.v1.websocket.BaseRecognizeCallback;
 
 public class Application {
+	public static String OI = "oi";
+	public static String AJUDA = "ajuda";
+	public static String LOCALIZACAO = "cachoeira";
+	public static String NOME = "matheus";
+	public static String SIM = "sim";
+	public static String NAO = "nao";
 	public static File file1 = new File("1.wav");
     public static File file2 = new File("2.wav");
     public static File file3 = new File("3.wav");
@@ -23,42 +34,12 @@ public class Application {
     public static File file6 = new File("6.wav");
     public static File file7 = new File("7.wav");
     public static File file8 = new File("8.wav");
-    
+    public static final String response = "";
     public static void main(String[] args) throws FileNotFoundException, InterruptedException, Exception {
         /**
-         * TODO: Text to speech
-         */
-        /*TextToSpeech textToSpeech = new TextToSpeech();
-        textToSpeech.setUsernameAndPassword("eb657d72-4c20-4d27-b0b2-03bd2711f365", "60A18b20tDmz");
-        
-        try {
-            SynthesizeOptions synthesizeOptions = new SynthesizeOptions.Builder().text("Obrigada pelas informações.").accept("audio/wav")
-                .voice(Voice.PT_BR_ISABELAVOICE).build();
-            
-            InputStream inputStream = textToSpeech.synthesize(synthesizeOptions).execute();
-            InputStream in = WaveUtils.reWriteWaveHeader(inputStream);
-            
-            OutputStream out = new FileOutputStream("test11.wav");
-            byte[] buffer = new byte[1024];
-            int length;
-            while ((length = in.read(buffer)) > 0) {
-                out.write(buffer, 0, length);
-            }
-            
-            out.close();
-            in.close();
-            inputStream.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }*/
-        
-        
-        playSound(file1);
-        
-        /**
          * TODO: Speech to text
-         */
-       SpeechToText service = new SpeechToText();
+         */    	
+    	SpeechToText service = new SpeechToText();
         service.setUsernameAndPassword("8ef046d2-0b24-45c1-b12d-8b9e37cf535a", "w0jWHgRbtg46");
         
         // Signed PCM AudioFormat with 16kHz, 16 bit sample size, mono
@@ -82,15 +63,37 @@ public class Application {
             .audio(audio).contentType(HttpMediaType.AUDIO_RAW + "; rate=" + sampleRate).model("pt-BR_BroadbandModel").build();
         
         service.recognizeUsingWebSocket(options, new BaseRecognizeCallback() {
-
-        	@Override
+            @Override
             public void onTranscription(SpeechRecognitionResults speechResults) {
-            	System.out.println(speechResults);
+            	if(speechResults.getResults().get(0).isFinalResults()) {
+            		final String response = speechResults.getResults().get(0).getAlternatives().get(0).getTranscript().trim();
+            		if(OI.trim().equals(response.toString())) {
+            			playSound(file1);
+            		}
+            		if(AJUDA.trim().equals(response.toString())) {
+            			playSound(file2);
+            		}
+            		if(LOCALIZACAO.trim().equals(response.toString())) {
+            			playSound(file3);
+            		}
+            		if(NOME.trim().equals(response.toString())) {
+            			playSound(file4);
+            		}
+            		if(SIM.trim().equals(response.toString())) {
+            			playSound(file5);
+            		}
+            		if(NAO.trim().equals(response.toString())) {
+            			playSound(file8);
+            		}
+            		System.out.println(response);
+            		
+            		//System.out.println(response);
+            	}
             }
         });
         
         System.out.println("Listening to your voice for the next 30s...");
-        Thread.sleep(30 * 1000);
+        Thread.sleep(50 * 1000);
         
         // closing the WebSockets underlying InputStream will close the WebSocket itself.
         line.stop();
